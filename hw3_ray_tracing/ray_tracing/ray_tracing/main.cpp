bc4603530fa078b100fe8fef29639ce8803515dd
Mituian <NatumeMituan@gmail.com>
2020-11-08 17:40:41 +0800

    更改phong模型中的参数

--- hw3_ray_tracing/ray_tracing/ray_tracing/main.cpp
+++ hw3_ray_tracing/ray_tracing/ray_tracing/main.cpp
@@ -17,7 +17,7 @@
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-Camera camera(glm::vec3(0.0f, 0.0f, 5.0f));
+Camera camera(glm::vec3(0.0f, 1.0f, 3.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
@@ -25,7 +25,7 @@ bool firstMouse = true;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
-glm::vec3 lightPos(0.0f, 0.0f, 0.0f);
+glm::vec3 lightPos(0.0f, 3.0f, 0.0f);
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
@@ -160,7 +160,7 @@ int main() {
 		// world transformation
 		for (int i = 0; i < 3; ++i) {
 			model = glm::mat4(1.0f);
-			model = glm::translate(model, glm::vec3(-2 + i * 2, -2, 0));
+			model = glm::translate(model, glm::vec3(-1 + i * 1, 0, 0));
 			model = glm::scale(model, glm::vec3(5.0f));
 			lightingShader.setMat4("model", model);
 			models[i].Draw(lightingShader);
